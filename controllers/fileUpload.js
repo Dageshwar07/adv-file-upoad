2bb5ff1f09450986162abd2ce4e17238ad41ba89
Dageshwar07 <dageshmanik@gmail.com>
2023-11-29 12:55:03 +0530

    all good

---
+++ controllers/fileUpload.js
@@ -0,0 +1,172 @@
+const File = require("../models/File");
+const cloudinary = require("cloudinary").v2;
+
+exports.localFileUpload = async (req, res) => {
+  try {
+    const file = req.files.file;
+    console.log("FILE AAGYI JEE -> ", file);
+
+    let path =
+      __dirname + "/files/" + Date.now() + `.${file.name.split(".")[1]}`;
+    console.log("PATH-> ", path);
+
+    file.mv(path, (err) => {
+      console.log(err);
+    });
+
+    res.json({
+      success: true,
+      message: "Local File Uploaded Successfully",
+    });
+  } catch (error) {
+    console.log("Not able to upload the file on server");
+    console.log(error);
+  }
+};
+
+function isFileTypeSupported(type, supportedTypes) {
+  return supportedTypes.includes(type);
+}
+
+async function uploadFileToCloudinary(file, folder, quality) {
+  const options = { folder };
+  console.log("temp file path", file.tempFilePath);
+
+  if (quality) {
+    options.quality = quality;
+  }
+
+  options.resource_type = "auto";
+  return await cloudinary.uploader.upload(file.tempFilePath, options);
+}
+
+exports.imageUpload = async (req, res) => {
+  try {
+    const { name, tags, email } = req.body;
+    console.log(name, tags, email);
+
+    const file = req.files.imageFile;
+    console.log(file);
+
+    const supportedTypes = ["jpg", "jpeg", "png"];
+    const fileType = file.name.split(".")[1].toLowerCase();
+    console.log("File Type:", fileType);
+
+    if (!isFileTypeSupported(fileType, supportedTypes)) {
+      return res.status(400).json({
+        success: false,
+        message: "File format not supported",
+      });
+    }
+
+    console.log("Uploading ");
+    const response = await uploadFileToCloudinary(file, "Dageshwar");
+    console.log(response);
+
+    const fileData = await File.create({
+      name,
+      tags,
+      email,
+      imageUrl: response.secure_url,
+    });
+
+    res.json({
+      success: true,
+      imageUrl: response.secure_url,
+      message: "Image Successfully Uploaded",
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({
+      success: false,
+      message: "Something went wrong",
+    });
+  }
+};
+
+exports.videoUpload = async (req, res) => {
+  try {
+    const { name, tags, email } = req.body;
+    console.log(name, tags, email);
+
+    const file = req.files.videoFile;
+
+    const supportedTypes = ["mp4", "mov"];
+    const fileType = file.name.split(".")[1].toLowerCase();
+    console.log("File Type:", fileType);
+
+    if (!isFileTypeSupported(fileType, supportedTypes)) {
+      return res.status(400).json({
+        success: false,
+        message: "File format not supported",
+      });
+    }
+
+    console.log("Uploading");
+    const response = await uploadFileToCloudinary(file, "Dageshwar");
+    console.log(response);
+
+    const fileData = await File.create({
+      name,
+      tags,
+      email,
+      imageUrl: response.secure_url,
+    });
+
+    res.json({
+      success: true,
+      imageUrl: response.secure_url,
+      message: "Video Successfully Uploaded",
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({
+      success: false,
+      message: "Something went wrong",
+    });
+  }
+};
+
+exports.imageSizeReducer = async (req, res) => {
+  try {
+    const { name, tags, email } = req.body;
+    console.log(name, tags, email);
+
+    const file = req.files.imageFile;
+    console.log(file);
+
+    const supportedTypes = ["jpg", "jpeg", "png"];
+    const fileType = file.name.split(".")[1].toLowerCase();
+    console.log("File Type:", fileType);
+
+    if (!isFileTypeSupported(fileType, supportedTypes)) {
+      return res.status(400).json({
+        success: false,
+        message: "File format not supported",
+      });
+    }
+
+    console.log("Uploading");
+    const response = await uploadFileToCloudinary(file, "dageshwar", 90);
+    console.log(response);
+
+    const fileData = await File.create({
+      name,
+      tags,
+      email,
+      imageUrl: response.secure_url,
+    });
+
+    res.json({
+      success: true,
+      imageUrl: response.secure_url,
+      message: "Image Successfully Uploaded",
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({
+      success: false,
+      message: "Something went wrong",
+    });
+  }
+};
---
+++ models/File.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+const nodemailer = require("nodemailer");
+
+const fileSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+    },
+    imageUrl:{
+        type:String,
+    },
+    tags:{
+        type:String,
+    },
+    email:{
+        type:String,
+    }
+});
+
+
+fileSchema.post("save", async function(doc) {
+    try{
+        console.log("DOC",doc)
+
+        let transporter = nodemailer.createTransport({
+            host: process.env.MAIL_HOST,
+            auth:{
+                user:process.env.MAIL_USER,
+                pass: process.env.MAIL_PASS,
+            },
+        });
+
+        let info = await transporter.sendMail({
+            from:`Dageshwar`,
+            to: doc.email,
+            subject: "New File Uploaded on Cloudinary",
+            html:`<h2>Hello Jee</h2> <p>File Uploaded View here: <a href="${doc.imageUrl}">${doc.imageUrl}</a> </p>`,
+        })
+        
+        console.log("INFO", info);
+
+
+    }
+    catch(error) {
+        console.error(error);
+    }
+})
+
+
+const File = mongoose.model("File", fileSchema);
+module.exports = File;
